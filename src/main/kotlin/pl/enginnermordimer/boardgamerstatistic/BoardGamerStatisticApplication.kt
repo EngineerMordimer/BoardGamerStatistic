1321fc9e19659bb64237ef1e259c389b5c486bba
Maciej Chmieliński <maciek.chmielinski91@gmail.com>
2023-03-16 17:58:59 +0100

    init project

---
+++ src/main/kotlin/pl/enginnermordimer/boardgamerstatistic/BoardGamerStatisticApplication.kt
@@ -0,0 +1,11 @@
+package pl.enginnermordimer.boardgamerstatistic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardGamerStatisticApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardGamerStatisticApplication>(*args)
+}
---
+++ src/test/kotlin/pl/enginnermordimer/boardgamerstatistic/BoardGamerStatisticApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.enginnermordimer.boardgamerstatistic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardGamerStatisticApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
